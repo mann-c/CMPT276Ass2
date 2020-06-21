bfd0ebd749c268d573eddb3b1109e6121a41d1da
Chanpreet Mann <chanpreetmann@MacBook-Pro.local>
2020-06-21 14:33:28 -0700

    finished assn2 basics

--- index.js
+++ index.js
@@ -2,9 +2,87 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+var app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({extended:false}));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+
+  app.get('/main', (req, res)=>{
+    var getPersonQuery = `SELECT * FROM Person`;
+    pool.query(getPersonQuery, (error, result)=>{
+      if(error)
+        res.end(error);
+      
+      var results = {'rows':result.rows};
+      res.render('pages/main', results);
+    })
+  });
+
+  app.get('/peoplepage', (req, res) => {
+    var getPersonQuery = `SELECT * FROM Person`;
+    pool.query(getPersonQuery, (error, result)=>{
+      if(error)
+        res.end(error);
+      
+      var results = {'rows':result.rows};
+      res.render('pages/peoplepage', results);
+    })
+  });
+
+  app.post('/addperson', (req, res) => {
+    var name = req.body.name;
+    var size = req.body.size;
+    var height = req.body.height;
+    var type = req.body.type;
+    var number;
+
+    var getPersonQuery = `SELECT MAX(ID) FROM Person`;
+    pool.query(getPersonQuery, (error, result)=>{
+      if(error)
+        res.end(error);
+      
+      number = result.rows[0].max + 1;
+      pool.query(`insert into person values(${number},'${name}', ${size}, ${height}, '${type}')`, (error,result)=>{
+        if(error)
+          res.end(error);
+      })
+      res.redirect('/main');
+    })
+  });
+
+  app.post('/deleteperson', (req, res) => {
+    var id = req.body.id;
+
+    var deleteQuery = `delete from person where id=${id}`;
+    pool.query(deleteQuery, (error, result) => {
+      if(error)
+        res.end(error);
+    })
+    res.redirect('/main');
+  });
+
+  app.post('/editperson', (req, res) => {
+    var id = req.body.id;
+    var name = req.body.name;
+    var size = req.body.size;
+    var height = req.body.height;
+    var type = req.body.type;
+
+    var modifyQuery = `update person set name='${name}', size=${size}, height=${height}, type='${type}' where id=${id}`;
+    pool.query(modifyQuery, (error, result) => {
+      if(error)
+        res.end(error);
+    })
+    res.redirect('/main');
+  });
+
+  app.get('/', (req, res) => res.render('pages/index'));
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
